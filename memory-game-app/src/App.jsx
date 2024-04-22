cc8eee90d724353a4bfaaf2eb4237a4ee101fb5c
Veritaaaas <rodneyjamesmmaisog@iskolarngbayan.pup.edu.ph>
2024-04-22 15:08:16 +0800

    changed the title and the icon

--- memory-game-app/src/App.jsx
+++ memory-game-app/src/App.jsx
@@ -1,11 +1,12 @@
 import { Cards } from './Cards';
+import { Helmet } from 'react-helmet';
+import favicon from './assets/pokeball.png';
 import './App.css'
 
 function App() {
   
   return (
     <>
-      <link href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap" rel="stylesheet"></link>
       <Cards />
     </>
   )
