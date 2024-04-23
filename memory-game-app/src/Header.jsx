d0635a91df1dd63e5f2cb0f308fe37a1de01e986
Veritaaaas <rodneyjamesmmaisog@iskolarngbayan.pup.edu.ph>
2024-04-23 12:10:29 +0800

    fix the image not rendering issue

--- memory-game-app/src/Header.jsx
+++ memory-game-app/src/Header.jsx
@@ -1,4 +1,7 @@
 import React, { useState } from 'react';
+import lightBulb from './assets/light_bulb.png';
+import pokeball from './assets/pokeball.png';
+import close from './assets/close.png';
 
 function Header({ score, HighScore }) {
     
@@ -12,10 +15,10 @@ function Header({ score, HighScore }) {
         <>
             <header className="grid grid-cols-header mt-1 items-center mb-3">
                 <div>
-                    <img src="src/assets/light_bulb.png" className="h-14 cursor-pointer" onClick={() => showInfoHandler()}/>
+                    <img src={lightBulb} className="h-14 cursor-pointer" onClick={() => showInfoHandler()}/>
                 </div>
                 <div className="flex items-center">
-                    <img src="src/assets/pokeball.png" className="h-16"/>
+                    <img src={pokeball} className="h-16"/>
                     <h2 className="text-3xl text-center font-Press font-press-start text-white">
                         <span className="text-red-500">Poké</span>
                         Memoria</h2>
@@ -28,7 +31,7 @@ function Header({ score, HighScore }) {
             {showInfo &&
                 <div className="fixed inset-0 flex items-center justify-center z-10 bg-black bg-opacity-50">
                 <div className="bg-gray-300 p-6 rounded-3xl border-4 border-black shadow-lg w-3/4 max-w-lg relative">
-                    <img src='src/assets/close.png' className="h-6 absolute top-2 right-2 cursor-pointer" onClick={() => showInfoHandler()}/>
+                    <img src={close} className="h-6 absolute top-2 right-2 cursor-pointer" onClick={() => showInfoHandler()}/>
                   <p className="text-sm text-center font-Press font-press-start text-black">Click on a Pokémon to earn points, but don't click on the same Pokémon twice!</p>
                 </div>
               </div>
